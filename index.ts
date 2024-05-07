8806cc436f4ccc7ee3bdacdb2e67135e10e1dac0
mussaratshamsher <mussaratshamsher@github.com>
2024-05-07 06:40:59 -0700

    update version

--- index.ts
+++ index.ts
@@ -1,30 +1,61 @@
 #! /usr/bin/env node
 
-
 import inquirer from "inquirer"
 import chalk from "chalk"
-let todos = [];
 
-let condition = true;
-console.log(chalk.bold.green.underline("WELCOME in Todos list. \n"))
+console.log(chalk.bold.green.underline("WELCOME in Todos list. Plan Your day adding task. \n"))
+let todos: string[] = [];
 
-while(condition) {
- let addTask = await inquirer.prompt(
-    [
-        {
-         name: "todo",
-         type: "input",
-         message: (chalk.bold.yellow("What do you want to add in your Todos? \n"))
-        },{
-            name: "addMore",
-            type: "confirm",
-            message: (chalk.bold.cyan("Do you want to add more?")),
-            default: "false"
-        }
-    ]
- )
+async function createTodo (todos: string[]){
 
- todos.push(addTask.todo);
- condition = addTask.addMore;
-console.log(chalk.bold.magenta(todos));
-};
+    do {
+let ans = await inquirer.prompt({
+    type: "list",
+    name: "select",
+    message: (chalk.bold.magenta("Select desired operation")),
+    choices: ["Add", "Update", "View", "Delete", "Exit"]
+});
+if (ans.select =="Add") {
+    let addTodo = await inquirer.prompt({
+        type: "input",
+        name: "todo",
+        message: (chalk.bold.cyanBright("Add item..."))
+ });
+ todos.push(addTodo.todo)
+}
+if (ans.select =="Update") {
+    let updateTodo = await inquirer.prompt({
+        type: "list",
+        name: "todo",
+        message: (chalk.bold.green("select item for update")),
+        choices:todos.map(item => item)
+    })
+    let addTodo = await inquirer.prompt({
+        type: "input",
+        name: "todo",
+        message: (chalk.bold.blue("Add item...")),
+    })
+    let newTodos = todos.filter(val => val !== updateTodo.todo)
+    todos = [...newTodos,addTodo.todo]
+}
+if (ans.select =="View") {
+    console.log(todos)
+}
+if (ans.select =="Delete") {
+    let deleteTodo = await inquirer.prompt({
+    type: "list",
+    name: "todo",
+    message: (chalk.bold.red("select item to delete")),
+    choices:todos.map(item => item)
+})
+let newTodos = todos.filter(val => val !== deleteTodo.todo)
+todos = [...newTodos]
+
+}
+if (ans.select =="Exit") {
+    process.exit();    
+}
+
+    } while(true);
+}
+createTodo(todos);
